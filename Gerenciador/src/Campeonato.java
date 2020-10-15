f5041789b926e3e0435cb87b611c6c5e6da15f1b
Pablo Passarini <pablo4passarini@gmail.com>
2020-10-15 17:25:37 -0300

    att5

--- Gerenciador/src/Campeonato.java
+++ Gerenciador/src/Campeonato.java
@@ -1,5 +1,9 @@
 
 public class Campeonato {
-	String Data;
-	
+	String nome_evento;
+	String data_inicial, data_final;
+	int quantidade_equipes;
+	float premio;
+	String local;
+	String narrador1, narrador2;
 }
