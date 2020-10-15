30e0c40e6b891022b02e67e00d1df109b3b1e98d
Pablo Passarini <pablo4passarini@gmail.com>
2020-10-15 18:06:44 -0300

    att8

--- Gerenciador/src/Campeonato.java
+++ Gerenciador/src/Campeonato.java
@@ -6,9 +6,9 @@ public class Campeonato {
 	float premio;
 	String local;
 	String narrador1, narrador2;
-<<<<<<< HEAD
+
 	
-	String dados_Campeonato() {
+	public String dados_Campeonato() {
 		String dados = "\n\nNome do Evento: " + this.nome_evento;
 		dados += "\nData inicial: " + this.data_inicial + " || Data final: " + this.data_final;
 		dados += "\nQuantidade de equipes: " + this.quantidade_equipes;
@@ -17,6 +17,5 @@ public class Campeonato {
 		dados += "\nNarrador 1: " + this.narrador1 + " || Narrador 2: " + this.narrador2;
 		return dados;
 	}
-=======
->>>>>>> branch 'master' of https://github.com/PabloPassarini/Gerenciador_de_Campeonatos_EAsports.git
+
 }
--- Gerenciador/src/Ingressos.java
+++ Gerenciador/src/Ingressos.java
@@ -1,11 +1,11 @@
 
 public class Ingressos {
-<<<<<<< HEAD
+
 	float valor_inteira_vip, valor_inteira_pista, valor_meia_vip, valor_meia_pista;
 	int quantidade_pista, quantidade_vip;
 	String range_arquibancadas_vip, range_arquibancadas_pista; // exemplo: da poltrona 1 ate a 100 é vip
 	
-	String dados_ingressos() {
+	public String dados_ingressos() {
 		String dados = "\n\nQuantidade de ingressos vip: " + this.quantidade_vip + " || Quantidade de ingressos pista: " + this.quantidade_pista;
 		dados += "\nPreco inteira vip R$" +this.valor_inteira_vip + " || Preco meia vip R$" + this.valor_meia_vip;
 		dados += "\nPreco inteira pista R$" +this.valor_inteira_pista + "|| Preco meia pista R$" +this.valor_meia_pista;
@@ -13,11 +13,4 @@ public class Ingressos {
 		dados += "\nRange arquibancadas pista: " + this.range_arquibancadas_pista;
 		return dados;
 	}
-	
-	
-=======
-	String apresentador;
-	String narrador1, narrador2;
->>>>>>> branch 'master' of https://github.com/PabloPassarini/Gerenciador_de_Campeonatos_EAsports.git
-	
 }
