4b42adf247bdb9230093572f954e60ccb4b0f33d
Pablo Passarini <pablo4passarini@gmail.com>
2020-10-16 13:31:16 -0300

    Começo da principal.java

--- Gerenciador/src/Principal.java
+++ Gerenciador/src/Principal.java
@@ -1,11 +1,21 @@
-
+import java.util.Scanner;
 public class Principal {
 
 	public static void main(String[] args) {
+		Scanner ler = new Scanner(System.in);
+		
 		Cadastro_Jogador[] v= new Cadastro_Jogador[10];
-		for(int x=0;x!=10;x++) {
-			v[x].nome="Tomaz";
-			System.out.println(v[x]);
+		for(int i=0; i<10; i++) {
+			v[i] = new Cadastro_Jogador();
 		}
+		
+		
+		for(int i=0; i<10; i++) {
+			System.out.println("Informe o nome do jogador: ");
+			v[i].nome_jogador =  ler.next();
+			System.out.println(v[i].nome_jogador);
+		}
+		
+		
 	}
 }
