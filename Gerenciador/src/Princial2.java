61b635b14b05848713083e6ce6990b722422324d
Pablo Passarini <pablo4passarini@gmail.com>
2020-10-17 20:36:49 -0300

    asdasdas

---
+++ Gerenciador/src/Princial2.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class Princial2 {
+
+	public static void main(String[] args) {
+		Scanner ler = new Scanner(System.in);
+		
+		
+		int contador_ingressos = 0;
+		
+		
+		
+		Ingressos[] ing = new Ingressos[10];
+		for(int i=0; i<10; i++) {
+			ing[i] = new Ingressos();
+		}
+		
+		
+	}
+
+}
