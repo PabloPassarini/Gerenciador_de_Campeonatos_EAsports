58ca24697f4b87f7e61ba9c53e851174dfd19462
Tomaz Xavier Rossi <tomazxr@gmail.com>
2020-10-18 15:50:58 -0300

    asd

--- Gerenciador/src/Lista_jogador.java
+++ Gerenciador/src/Lista_jogador.java
@@ -1,32 +1,32 @@
-public class Lista {
+public class Lista_jogador { // Esboço
 	private Nodo prim;
 
 
-	public void criaLista(){//Cria uma lista
+	public void criaLista_jogaodr(){//Cria uma lista
 		prim = null;
 	}
 
-	public void add(int i){//adiciona elemento na lista
+	public void add_jogador(int i){//adiciona elemento na lista
 		Nodo novo = new Nodo();
 		novo.setInfo(i);
 		novo.setProx(prim);
 		prim = novo;
 	}
 
-	public void print(){ //imprime lista
+	public void print_jogador(){ //imprime lista
 		for(Nodo n = prim; n != null; n = n.getProx()){
 			System.out.println(n.getInfo());
 		}
 	}
 
-	public boolean isEmpty(){ // verificar se lista esta fazia
+	public boolean isEmpty_jogador(){ // verificar se lista esta fazia
 		if(prim == null)
 			return true;
 		else
 			return false;
 	}
 
-	public Nodo search(int i){//Procurar na lista
+	public Nodo search_jogador(int i){//Procurar na lista
 		for(Nodo n = prim; n != null; n = n.getProx()){
 			if(n.getInfo()==i){
 				return n;
@@ -35,7 +35,7 @@ public class Lista {
 		return null; 
 	}
 	
-	public void remove(int i){ //Remover elemento
+	public void remove_jogador(int i){ //Remover elemento
 		Nodo anterior = null;
 		Nodo p = prim;
 		while(p != null && p.getInfo() != i){
@@ -51,6 +51,5 @@ public class Lista {
 			anterior.setProx(p.getProx());
 		}
 	}
-	
 }
 
