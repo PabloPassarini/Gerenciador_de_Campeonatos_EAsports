8582f57f4deb8fc3d32145f485a4a480c04ec43d
Tomaz Xavier Rossi <tomazxr@gmail.com>
2020-10-18 16:01:20 -0300

    asd

--- Gerenciador/src/Cadastro_Jogador.java
+++ Gerenciador/src/Cadastro_Jogador.java
@@ -1,14 +1,40 @@
 
 public class Cadastro_Jogador {
-	public String nome;
-	String nome_jogador,CPF_jogador, equipe, funcao, morador;
+	String nome_jogador,CPF_jogador, equipe_jogador;
+	//String funcao, morador;
 	int idade_jogador;
 	String dados_jogadores() {
 		String dados = "\n Nome do jogador; " + this.nome_jogador;
 		dados+= "\nCPF: " + this.CPF_jogador;
-		dados+= "\nEquipe: " + this.equipe;
-		dados+= "\nFuncao: " + this.funcao;
-		dados+= "\nMorador da cidade; " +this.morador;
+		dados+= "\nEquipe: " + this.equipe_jogador;
+		//dados+= "\nFuncao: " + this.funcao;
+		//dados+= "\nMorador da cidade; " +this.morador;
 		return dados;
+	}
+	private Cadastro_Jogador prox;
+
+	public String getInfo_nome_jogador() {
+		return nome_jogador;
+	}
+	public int getInfo_idade_jogador() {
+		return idade_jogador;
+	}
+	public String getInfo_CPF_jogador() {
+		return CPF_jogador;
+	}
+	public String getInfo_equipe_jogador() {
+		return equipe_jogador;
+	}
+	public void setInfo(String nome_jogador,int idade_jogador,String CPF_jogador,String equipe_jogador) {
+		this.nome_jogador = nome_jogador;
+		this.idade_jogador = idade_jogador;
+		this.CPF_jogador = CPF_jogador;
+		this.equipe_jogador = equipe_jogador;
+	}
+	public Cadastro_Jogador getProx() {
+		return prox;
+	}
+	public void setProx(Cadastro_Jogador prox) {
+		this.prox = prox;
 	}
 }
--- Gerenciador/src/Lista_jogador.java
+++ Gerenciador/src/Lista_jogador.java
@@ -1,21 +1,22 @@
 public class Lista_jogador { // Esboço
-	private Nodo prim;
+	private Cadastro_Jogador prim;
 
 
 	public void criaLista_jogaodr(){//Cria uma lista
 		prim = null;
 	}
 
-	public void add_jogador(int i){//adiciona elemento na lista
-		Nodo novo = new Nodo();
-		novo.setInfo(i);
+	public void add_jogador(String n,int i,String c,String e){//adiciona elemento na lista
+		Cadastro_Jogador novo = new Cadastro_Jogador();
+		novo.setInfo(n,i,c,e);
 		novo.setProx(prim);
 		prim = novo;
 	}
 
 	public void print_jogador(){ //imprime lista
-		for(Nodo n = prim; n != null; n = n.getProx()){
-			System.out.println(n.getInfo());
+		for(Cadastro_Jogador n = prim; n != null; n = n.getProx()){
+			n.dados_jogadores();
+			System.out.println("\n");
 		}
 	}
 
@@ -26,19 +27,28 @@ public class Lista_jogador { // Esboço
 			return false;
 	}
 
-	public Nodo search_jogador(int i){//Procurar na lista
-		for(Nodo n = prim; n != null; n = n.getProx()){
-			if(n.getInfo()==i){
-				return n;
+	public Cadastro_Jogador search_jogador_nome(String n){//Procurar na lista pelo nome
+		for(Cadastro_Jogador i = prim; i != null; i = i.getProx()){
+			if(i.getInfo_nome_jogador()==n){
+				return i;
 			}
 		}
 		return null; 
 	}
-	
-	public void remove_jogador(int i){ //Remover elemento
-		Nodo anterior = null;
-		Nodo p = prim;
-		while(p != null && p.getInfo() != i){
+
+	public Cadastro_Jogador search_jogador_CPF(String n){//Procurar na lista pelo CPF
+		for(Cadastro_Jogador i = prim; i != null; i = i.getProx()){
+			if(i.getInfo_CPF_jogador()==n){
+				return i;
+			}
+		}
+		return null; 
+	}
+
+	public void remove_jogador_nome(String n){ //Remover elemento conforme o nome
+		Cadastro_Jogador anterior = null;
+		Cadastro_Jogador p = prim;
+		while(p != null && p.getInfo_nome_jogador() != n){
 			anterior = p;
 			p = p.getProx();
 		}
@@ -47,9 +57,26 @@ public class Lista_jogador { // Esboço
 		}
 		if(anterior == null){
 			prim = p.getProx();
-		}else{
+		}
+		else{
 			anterior.setProx(p.getProx());
 		}
 	}
+public void remove_jogador_CPF(String n){ //Remover elemento conforme o CPF
+	Cadastro_Jogador anterior = null;
+	Cadastro_Jogador p = prim;
+	while(p != null && p.getInfo_CPF_jogador() != n){
+		anterior = p;
+		p = p.getProx();
+	}
+	if(p == null){
+		return;
+	}
+	if(anterior == null){
+		prim = p.getProx();
+	}else{
+		anterior.setProx(p.getProx());
+	}
+}
 }
 
