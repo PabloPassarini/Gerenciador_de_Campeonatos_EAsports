b43136e489e235e299174fa1cd9537deb7c39cac
Tomaz Xavier Rossi <tomazxr@gmail.com>
2020-10-18 16:43:31 -0300

    dsa

--- Gerenciador/src/Lista_jogador.java
+++ Gerenciador/src/Lista_jogador.java
@@ -1,34 +1,34 @@
 public class Lista_jogador { // Esboço
-	private Cadastro_Jogador prim;
+	private Cadastro_Jogador prim_jogador;
 
 
 	public void criaLista_jogaodr(){//Cria uma lista
-		prim = null;
+		prim_jogador = null;
 	}
 
 	public void add_jogador(String n,int i,String c,String e){//adiciona elemento na lista
 		Cadastro_Jogador novo = new Cadastro_Jogador();
 		novo.setInfo(n,i,c,e);
-		novo.setProx(prim);
-		prim = novo;
+		novo.setProx(prim_jogador);
+		prim_jogador = novo;
 	}
 
 	public void print_jogador(){ //imprime lista
-		for(Cadastro_Jogador n = prim; n != null; n = n.getProx()){
+		for(Cadastro_Jogador n = prim_jogador; n != null; n = n.getProx()){
 			n.dados_jogadores();
 			System.out.println("\n");
 		}
 	}
 
 	public boolean isEmpty_jogador(){ // verificar se lista esta fazia
-		if(prim == null)
+		if(prim_jogador == null)
 			return true;
 		else
 			return false;
 	}
 
 	public Cadastro_Jogador search_jogador_nome(String n){//Procurar na lista pelo nome
-		for(Cadastro_Jogador i = prim; i != null; i = i.getProx()){
+		for(Cadastro_Jogador i = prim_jogador; i != null; i = i.getProx()){
 			if(i.getInfo_nome_jogador()==n){
 				return i;
 			}
@@ -37,7 +37,7 @@ public class Lista_jogador { // Esboço
 	}
 
 	public Cadastro_Jogador search_jogador_CPF(String n){//Procurar na lista pelo CPF
-		for(Cadastro_Jogador i = prim; i != null; i = i.getProx()){
+		for(Cadastro_Jogador i = prim_jogador; i != null; i = i.getProx()){
 			if(i.getInfo_CPF_jogador()==n){
 				return i;
 			}
@@ -47,7 +47,7 @@ public class Lista_jogador { // Esboço
 
 	public void remove_jogador_nome(String n){ //Remover elemento conforme o nome
 		Cadastro_Jogador anterior = null;
-		Cadastro_Jogador p = prim;
+		Cadastro_Jogador p = prim_jogador;
 		while(p != null && p.getInfo_nome_jogador() != n){
 			anterior = p;
 			p = p.getProx();
@@ -56,7 +56,7 @@ public class Lista_jogador { // Esboço
 			return;
 		}
 		if(anterior == null){
-			prim = p.getProx();
+			prim_jogador = p.getProx();
 		}
 		else{
 			anterior.setProx(p.getProx());
@@ -64,7 +64,7 @@ public class Lista_jogador { // Esboço
 	}
 public void remove_jogador_CPF(String n){ //Remover elemento conforme o CPF
 	Cadastro_Jogador anterior = null;
-	Cadastro_Jogador p = prim;
+	Cadastro_Jogador p = prim_jogador;
 	while(p != null && p.getInfo_CPF_jogador() != n){
 		anterior = p;
 		p = p.getProx();
@@ -73,7 +73,7 @@ public void remove_jogador_CPF(String n){ //Remover elemento conforme o CPF
 		return;
 	}
 	if(anterior == null){
-		prim = p.getProx();
+		prim_jogador = p.getProx();
 	}else{
 		anterior.setProx(p.getProx());
 	}
