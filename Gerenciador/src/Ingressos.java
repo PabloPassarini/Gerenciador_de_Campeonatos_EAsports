399003819b130820f2c77bfe88dfd8d70a288538
Pablo Passarini <pablo4passarini@gmail.com>
2020-10-19 17:08:36 -0300

    seila

--- Gerenciador/src/Ingressos.java
+++ Gerenciador/src/Ingressos.java
@@ -1,94 +1,67 @@
 
 public class Ingressos {
-	private String nome_evento;
-	private float valor_inteira_vip, valor_inteira_pista, valor_meia_vip, valor_meia_pista;
-	private int quantidade_pista, quantidade_vip;
-	private String range_arquibancadas_vip, range_arquibancadas_pista; // exemplo: da poltrona 1 ate a 100 é vip
+	String comprador;
+	int cpf, qtd_inteira, qtd_meia;
+	float valor_meias, valor_inteira, valor_total;
 	
-	public String getNome_evento() {
-		return nome_evento;
+	
+	
+	public String getComprador() {
+		return comprador;
 	}
-	public void setNome_evento(String nome_evento) {
-		this.nome_evento = nome_evento;
+	public void setComprador(String comprador) {
+		this.comprador = comprador;
 	}
-	public float getValor_inteira_vip() {
-		return valor_inteira_vip;
+	public int getCpf() {
+		return cpf;
 	}
-	public void setValor_inteira_vip(float valor_inteira_vip) {
-		this.valor_inteira_vip = valor_inteira_vip;
+	public void setCpf(int cpf) {
+		this.cpf = cpf;
 	}
-	public float getValor_inteira_pista() {
-		return valor_inteira_pista;
+	public int getQtd_inteira() {
+		return qtd_inteira;
 	}
-	public void setValor_inteira_pista(float valor_inteira_pista) {
-		this.valor_inteira_pista = valor_inteira_pista;
+	public void setQtd_inteira(int qtd_inteira) {
+		this.qtd_inteira = qtd_inteira;
 	}
-	public float getValor_meia_vip() {
-		return valor_meia_vip;
+	public int getQtd_meia() {
+		return qtd_meia;
 	}
-	public void setValor_meia_vip(float valor_meia_vip) {
-		this.valor_meia_vip = valor_meia_vip;
-	}
-	public float getValor_meia_pista() {
-		return valor_meia_pista;
-	}
-	public void setValor_meia_pista(float valor_meia_pista) {
-		this.valor_meia_pista = valor_meia_pista;
-	}
-	public int getQuantidade_pista() {
-		return quantidade_pista;
-	}
-	public void setQuantidade_pista(int quantidade_pista) {
-		this.quantidade_pista = quantidade_pista;
-	}
-	public int getQuantidade_vip() {
-		return quantidade_vip;
-	}
-	public void setQuantidade_vip(int quantidade_vip) {
-		this.quantidade_vip = quantidade_vip;
-	}
-	public String getRange_arquibancadas_vip() {
-		return range_arquibancadas_vip;
-	}
-	public void setRange_arquibancadas_vip(String range_arquibancadas_vip) {
-		this.range_arquibancadas_vip = range_arquibancadas_vip;
-	}
-	public String getRange_arquibancadas_pista() {
-		return range_arquibancadas_pista;
-	}
-	public void setRange_arquibancadas_pista(String range_arquibancadas_pista) {
-		this.range_arquibancadas_pista = range_arquibancadas_pista;
-	}
-	public String getApresentador() {
-		return apresentador;
-	}
-	public void setApresentador(String apresentador) {
-		this.apresentador = apresentador;
-	}
-	public String getNarrador1() {
-		return narrador1;
-	}
-	public void setNarrador1(String narrador1) {
-		this.narrador1 = narrador1;
-	}
-	public String getNarrador2() {
-		return narrador2;
-	}
-	public void setNarrador2(String narrador2) {
-		this.narrador2 = narrador2;
+	public void setQtd_meia(int qtd_meia) {
+		this.qtd_meia = qtd_meia;
 	}
 	
 	
 	
-	public String dados_ingressos() {
-		String dados = "\n\nQuantidade de ingressos vip: " + this.quantidade_vip + " || Quantidade de ingressos pista: " + this.quantidade_pista;
-		dados += "\nPreco inteira vip R$" +this.valor_inteira_vip + " || Preco meia vip R$" + this.valor_meia_vip;
-		dados += "\nPreco inteira pista R$" +this.valor_inteira_pista + "|| Preco meia pista R$" +this.valor_meia_pista;
-		dados += "\nRange arquibancadas vip: " + this.range_arquibancadas_vip;
-		dados += "\nRange arquibancadas pista: " + this.range_arquibancadas_pista;
-		return dados;
+	
+	
+	
+	
+	
+	
+	public void calculavalor(int tipo, float valor) {
+		if(tipo == 1) { // 1 - Vip
+			this.valor_inteira = this.qtd_inteira * valor;
+		}
+		if(tipo == 2){
+			this.valor_meias = this.qtd_meia * valor;
+		}
 	}
-	String apresentador;
-	String narrador1, narrador2;
+	
+	public void finalizar() {
+		this.valor_total = this.valor_inteira + this.valor_meias;
+	}
+	public String dados() {
+		String d = "\nComprador" + this.comprador;
+		d += "\nCpf: " + this.cpf;
+		d += "\nQuantidade de ingressos do tipo inteiro: " + this.qtd_inteira + " || Valor R$"+this.valor_inteira;
+		d += "\nQuantidade de ingressos do tipo meia: " + this.qtd_meia+ " || Valor R$"+this.valor_meias;
+		d +="\nValor total R$" + (this.valor_total);
+		
+		return d;
+	}
+	
+	
+	
 
 }
--- Gerenciador/src/Principal.java
+++ Gerenciador/src/Principal.java
@@ -2,6 +2,102 @@ import java.util.Scanner;
 public class Principal {
 
 	public static void main(String[] args) {
+		int op = -1,op_edit, cont_ing = 0, qtd_ing =10, aux;
+		String aux_s;
+		Scanner ler = new Scanner(System.in);
+		
+		
+		
+		
+		//Vetor para a classe Ingressos
+		Ingressos[] ing = new Ingressos[10];
+		for(int i=0; i<10; i++) {
+			ing[i] = new Ingressos();
+		}
+		while(op != 0) {
+			System.out.println("*******************Menu*********************");
+			System.out.println("1-Adicionar Jogador, 2-Ingresso,3-Campeonato,4-Patrocinio, 5- , 6-Editar ingresso");
+			op = ler.nextInt();
+			if(op == 1) {
+				
+				
+				
+			}else if(op == 2) {
+				if(qtd_ing != 0) {
+					int q1, q2;
+					System.out.println("Temos apenas " + qtd_ing + " ");
+					System.out.println("Digite o nome do comprador: ");
+					ing[cont_ing].comprador = ler.next();
+					System.out.println("Digite o cpf (somente os numeros): ");
+					ing[cont_ing].cpf = ler.nextInt();
+					System.out.println("Quantidade de ingressos inteira (0 para nenhum): ");
+					q1 = ler.nextInt();
+					ing[cont_ing].qtd_inteira = q1;
+					System.out.println("Quantidade de ingresso meia (0 para meia): ");
+					q2 = ler.nextInt();
+					ing[cont_ing].qtd_meia = q2;
+					ing[cont_ing].calculavalor(1, 30);
+					ing[cont_ing].calculavalor(2, 15);
+					ing[cont_ing].finalizar();
+					System.out.println(ing[cont_ing].dados());
+				}else {
+					System.out.println("Ingressos exgotados");
+				}		
+			}else if(op == 4) {
+				
+				
+			}else if(op == 5) {
+				
+				
+			}else if(op == 6) {
+				System.out.println("Procurar ingresso, digite o cpf: ");
+				aux = ler.nextInt();
+				int pos = -1;
+				for(int i=0; i<10; i++) {
+					if(ing[i].getCpf() == aux) {
+						pos = i;
+						System.out.println(ing[i].getComprador());
+						break;
+					}
+				}
+				
+				if(pos != -1) {
+				while(true){
+					
+						op_edit = 0;
+						System.out.println("Editar:\n1-Nome comprador, 2-Quantidade ingressos inteira, 3-Quantidade ingresso meia, 4-Imprimir dados, 5-Sair");
+						op_edit = ler.nextInt();
+						if(op_edit == 1) {
+							System.out.println("Novo nome: ");
+							aux_s = ler.next();
+							ing[pos].setComprador(aux_s);
+						}else if(op_edit == 2) {
+							System.out.println("Quantiade ingressos inteira: ");
+							aux = ler.nextInt();
+							ing[pos].setQtd_inteira(aux);
+						}else if(op_edit == 3) {
+							System.out.println("Quantiade ingressos meia: ");
+							aux = ler.nextInt();
+							ing[pos].setQtd_meia(aux);
+						}else if(op_edit == 4) {
+							System.out.println(ing[pos].dados());
+						}else if(op_edit == 5) {
+							break;
+						}
+					}
+					
+				}else {
+					System.out.println("Ingresso não encontrado! ");
+				}
+				
+			}else if(op == 7) {
+				
+				
+			}
+		}
+		
+		
+		
 		
 	}
 }
