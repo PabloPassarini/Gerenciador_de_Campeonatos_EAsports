b745cc2760fe74624e6133057ac387ef6f686456
Tomaz Xavier Rossi <tomazxr@gmail.com>
2020-10-20 18:13:14 -0300

    sad

--- Gerenciador/src/testes.java
+++ Gerenciador/src/testes.java
@@ -1,87 +1,43 @@
 import java.util.Scanner;
 public class testes {
 
-/*	public static void main(String[] args) {
-		int aux=0;
-		 Scanner ler = new Scanner(System.in);
-
-
-
-		 Lista_Patrocinio lista = new Lista_Patrocinio();
-		 lista.criaLista_patrocinio();
-
-		while(aux!=5)
-		{
-			System.out.println("1->Add;2->Print;3->Search;4->Remove;5->Exit");
-			aux=ler.nextInt();
-			if(aux==1)
-			{
-				String aa="tomaz";
-				String bb="pablo";
-				lista.add_patrocinio(2000,aa);
-				lista.add_patrocinio(500,bb);
-			}
-			if(aux==2)
-			{
-				lista.print_patrocinio();
-			}
-			if(aux==3)//Funciona
-			{
-				String aa="tomaz";
-				patrocinio x=Lista_Patrocinio.search_patrocinio(aa);
-				System.out.println(x.getEmpresa_patrocinio());
-				System.out.println(x.getvalor_patrocinio());
-		}
-			if(aux==4)
-			{
-				String bb="pablo";
-				lista.remove_patrocinio(bb);
-			}
-	}
-		}
-	}*/
-
-
 	public static void main(String[] args) {
 		int aux=0;
 		Scanner ler = new Scanner(System.in);
 
 
 
-		Lista_Patrocinio lista = new Lista_Patrocinio();
-		lista.criaLista_patrocinio();
+		Lista_equipe lista = new Lista_equipe();
+		lista.criaLista_equipe();
+
 		while(aux!=5)
 		{
-			System.out.println("1->Add;2->Print;3->Search;4->Remove;5->Exit");
+			System.out.println("1->Add; 2->Print; 3->Search; 4->Remove; 5->Exit");
 			aux=ler.nextInt();
 			if(aux==1)
 			{
-				String bb;
-				double aa;
-				System.out.println("Digite o nome da Empresa");
-				bb=ler.next();
-				System.out.println("Digite o valor contribuido pela empresa");
-				aa=ler.nextDouble();
-				lista.add_patrocinio(aa,bb);
+				String aa="tomaz";//adiciona equipe com o nome tomaz
+				String bb="pablo";//adiciona equipe com o nome pablo
+				
+				
+				lista.add_equipe(aa);
+				lista.add_equipe(bb);
 			}
 			if(aux==2)
 			{
-				lista.print_patrocinio();
+				lista.print_equipe();
 			}
-			if(aux==3)//nao funciona
+			if(aux==3)
 			{
-				String  aa;
-				System.out.println("Digite o nome da Empresa");
-				aa=ler.next();
-				System.out.println(lista.search_patrocinio(aa).getvalor_patrocinio());
+				String aa="tomaz";//procura equipe com o nome tomaz
+				Equipe x=lista.search_equipe(aa);
+				System.out.println(x.getnome_equipe());
 			}
-			if(aux==4)//nao funciona
+			if(aux==4)
 			{
-				String bb;
-				System.out.println("Digite o nome da empresa que deseje remover");
-				bb=ler.next();
-				lista.remove_patrocinio(bb);
+				String bb="pablo"; // remove equipe com o nome pablo
+				lista.remove_equipe(bb);
+			}
 			}
 		}
 	}
-}
