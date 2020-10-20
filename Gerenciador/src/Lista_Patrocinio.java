2bc9eb83d3a9a2d66c2453e1d11d38fd56a6c28a
Tomaz Xavier Rossi <tomazxr@gmail.com>
2020-10-19 21:24:24 -0300

    asd

--- Gerenciador/src/Lista_Patrocinio.java
+++ Gerenciador/src/Lista_Patrocinio.java
@@ -1,54 +1,54 @@
 public class Lista_Patrocinio {
 
-	
-		 private patrocinio prim;
-		 public void criaLista(){
-		 prim = null;
-		 }
-	
-	
+
+	private patrocinio prim;
+	public void criaLista(){
+		prim = null;
+	}
+
+
 	public void add_patrocinio(double i,String  n){
 		patrocinio novo = new patrocinio();
-		 novo.setValor_patrocinio(i);
-		 novo.setEmpresa_patrocinio(n);
-		 novo.setProx(prim);
-		 prim = novo;
-		 }
-	
-	
+		novo.setValor_patrocinio(i);
+		novo.setEmpresa_patrocinio(n);
+		novo.setProx(prim);
+		prim = novo;
+	}
+
+
 	public void print_patrocinio(){
-		 for(patrocinio n = prim; n != null; n = n.getProx()){
-		 System.out.println("Nome: "+n.getEmpresa_patrocinio());
-		 System.out.println("Contribuiçao: "+n.getvalor_patrocinio());
-		 }
-		 }
-	
-	
+		for(patrocinio n = prim; n != null; n = n.getProx()){
+			System.out.println("Nome: "+n.getEmpresa_patrocinio());
+			System.out.println("Contribuiçao: "+n.getvalor_patrocinio());
+		}
+	}
+
+
 	public patrocinio search_patrocinio(String i){
-		 for(patrocinio n = prim; n != null; n = n.getProx()){
-		 if(n.getEmpresa_patrocinio()==i){
-			 System.out.println("Nome: "+n.getEmpresa_patrocinio());
-			 System.out.println("Contribuiçao: "+n.getvalor_patrocinio());
-		 }
-		 }
-		 return null;
-		 }
-	
-	
+		for(patrocinio n = prim; n != null; n = n.getProx()){
+			if(n.getEmpresa_patrocinio()==i){
+				System.out.println("Nome: "+n.getEmpresa_patrocinio());
+				System.out.println("Contribuiçao: "+n.getvalor_patrocinio());
+			}
+		}
+		return null;
+	}
+
+
 	public void remove_patrocinio(String i){
 		patrocinio anterior = null;
 		patrocinio p = prim;
-		 while(p != null && p.getEmpresa_patrocinio() != i){
-		 anterior = p;
-		 p = p.getProx();
-		 }
-		 if(p == null){
-		 return;
-		 }
-		 if(anterior == null){
-		 prim = p.getProx();
-		 }else{
-		 anterior.setProx(p.getProx());
-		 }
-		 }
+		while(p != null && p.getEmpresa_patrocinio() != i){
+			anterior = p;
+			p = p.getProx();
+		}
+		if(p == null){
+			return;
+		}
+		if(anterior == null){
+			prim = p.getProx();
+		}else{
+			anterior.setProx(p.getProx());
+		}
+	}
 }
